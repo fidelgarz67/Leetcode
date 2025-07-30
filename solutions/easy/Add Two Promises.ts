6fb31e1885a2303d5666fcd0fd79e9c26c4aeca7
Fidel Garcia <54052260+fidelgarz67@users.noreply.github.com>
2025-07-29 21:09:06 -0600

    Add solution for Add Two Promises

---
+++ solutions/easy/Add Two Promises.ts
@@ -0,0 +1,15 @@
+// Title: Add Two Promises
+            // Difficulty: Easy
+            // Language: TypeScript
+            // Link: https://leetcode.com/problems/add-two-promises/
+
+type P = Promise<number>
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+    return await promise1 + await promise2
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
