5f6f05509d7c10eb13a68b57a34b4da49310b0f7
Fidel Garcia <54052260+fidelgarz67@users.noreply.github.com>
2025-07-31 01:09:54 -0600

    Update Score of a String.ts
    Update formatting

--- solutions/easy/Score of a String.ts
+++ solutions/easy/Score of a String.ts
@@ -3,13 +3,13 @@
             // Language: TypeScript
             // Link: https://leetcode.com/problems/score-of-a-string/
 
-function scoreOfString(s: string): number {
-    var sum = 0
-    for(var i=1; i<s.length; i++){
-        var score = s.charCodeAt(i-1)-s.charCodeAt(i)
-    }  
+function scoreOfString(s: string): number {
+    var sum = 0
+    for(var i=1; i<s.length; i++){
+        var score = s.charCodeAt(i-1)-s.charCodeAt(i)
 
-        if(score > 0){ sum += score}
-    return sum;
-        else {sum += (score * -1)}
+    if(score > 0){ sum += score}
+        else {sum += (score * -1)}
+    }
+    return sum;
 };
