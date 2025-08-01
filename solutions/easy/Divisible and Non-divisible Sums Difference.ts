0bafd8d107025bdbe3fa53bc6237275ecac023d4
Fidel Garcia <54052260+fidelgarz67@users.noreply.github.com>
2025-07-31 21:31:10 -0600

    Update Divisible and Non-divisible Sums Difference.ts
    adjust the code formatting

--- solutions/easy/Divisible and Non-divisible Sums Difference.ts
+++ solutions/easy/Divisible and Non-divisible Sums Difference.ts
@@ -3,13 +3,13 @@
             // Language: TypeScript
             // Link: https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
 
-function differenceOfSums(n: number, m: number): number {
-    var num1 = 0
-    for(var i = 1; i <= n; i++){
-        if(i % m !== 0){ num1 += i }
-            else { num2 += i}
-    return num1 - num2
-    }
+function differenceOfSums(n: number, m: number): number {
+    var num1 = 0
+    var num2 = 0
 
-    var num2 = 0
+    for(var i = 1; i <= n; i++){
+        if(i % m !== 0){ num1 += i }
+            else { num2 += i}
+    }
+    return num1 - num2
 };
