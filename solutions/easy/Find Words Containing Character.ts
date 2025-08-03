8d7c81b91be3baf76c6ece6faa4b0e11c92ba98d
Fidel Garcia Villa Jr. <54052260+fidelgarz67@users.noreply.github.com>
2025-08-03 01:44:01 -0600

    Add solution for Find Words Containing Character

---
+++ solutions/easy/Find Words Containing Character.ts
@@ -0,0 +1,16 @@
+// Title: Find Words Containing Character
+            // Difficulty: Easy
+            // Language: TypeScript
+            // Link: https://leetcode.com/problems/find-words-containing-character/
+
+function findWordsContaining(words: string[], x: string): number[] {
+    for(let i = 0; i < words.length; i++){
+        for(let j = 0; j < [...words[i]].length; j++){
+    }
+
+        }
+    return [... new Set(ans)]
+    let ans:number[] = []
+            if([...words[i]][j] === x){ ans.push (i)}
+    // new sets will remove duplicate variables
+};
