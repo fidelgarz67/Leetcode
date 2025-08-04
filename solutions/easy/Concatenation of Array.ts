ec3786c7b1e5da302b933874927996d4155a4faa
Fidel Garcia Villa Jr. <54052260+fidelgarz67@users.noreply.github.com>
2025-08-03 18:09:03 -0600

    Update Concatenation of Array.ts
    Adjust the indention

--- solutions/easy/Concatenation of Array.ts
+++ solutions/easy/Concatenation of Array.ts
@@ -3,16 +3,14 @@
             // Language: TypeScript
             // Link: https://leetcode.com/problems/concatenation-of-array/
 
-function getConcatenation(nums: number[]): number[] {
-    let ans:number[] = []
+function getConcatenation(nums: number[]): number[] {
+    let ans:number[] = []
+    for(let i = 0; i < nums.length; i++){
+        ans.push(nums[i])
+    }
 
-    for(let i = 0; i < nums.length; i++){
-
-    }
-        ans.push(nums[i])
-
-    for(let j = 0; j < nums.length; j++){
-        ans.push(nums[j])
-    }
-    return ans
+    for(let j = 0; j < nums.length; j++){
+        ans.push(nums[j])
+    }
+    return ans
 };
