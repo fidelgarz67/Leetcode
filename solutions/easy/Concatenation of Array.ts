06f992077702319613a5f38b19c94a741c7b11ec
Fidel Garcia Villa Jr. <54052260+fidelgarz67@users.noreply.github.com>
2025-08-03 18:07:29 -0600

    Add solution for Concatenation of Array

---
+++ solutions/easy/Concatenation of Array.ts
@@ -0,0 +1,18 @@
+// Title: Concatenation of Array
+            // Difficulty: Easy
+            // Language: TypeScript
+            // Link: https://leetcode.com/problems/concatenation-of-array/
+
+function getConcatenation(nums: number[]): number[] {
+    let ans:number[] = []
+
+    for(let i = 0; i < nums.length; i++){
+
+    }
+        ans.push(nums[i])
+
+    for(let j = 0; j < nums.length; j++){
+        ans.push(nums[j])
+    }
+    return ans
+};
