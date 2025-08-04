ff0e651f4e699af8e084fb17549f70f92c79e868
Fidel Garcia Villa Jr. <54052260+fidelgarz67@users.noreply.github.com>
2025-08-03 23:12:32 -0600

    Update Convert the Temperature.ts
    Adjust indention

--- solutions/easy/Convert the Temperature.ts
+++ solutions/easy/Convert the Temperature.ts
@@ -3,9 +3,9 @@
             // Language: TypeScript
             // Link: https://leetcode.com/problems/convert-the-temperature/
 
-function convertTemperature(celsius: number): number[] {
-  let ans:number[] = []
-  ans.push(celsius + 273.15) 
-    return ans
-  ans.push(celsius * 1.80 + 32.00)
+function convertTemperature(celsius: number): number[] {
+  let ans:number[] = []
+  ans.push(celsius + 273.15) 
+  ans.push(celsius * 1.80 + 32.00)
+return ans
 };
