031062f16ad6a2918e4750277960ccc93c11816a
Fidel Garcia Villa Jr. <54052260+fidelgarz67@users.noreply.github.com>
2025-08-04 20:24:47 -0600

    Update Number of Good Pairs.ts
    Adjust the indention

--- solutions/easy/Number of Good Pairs.ts
+++ solutions/easy/Number of Good Pairs.ts
@@ -3,14 +3,14 @@
             // Language: TypeScript
             // Link: https://leetcode.com/problems/number-of-good-pairs/
 
-function numIdenticalPairs(nums: number[]): number {
-    let ans:number = 0
-    for(let i = 0; i < nums.length; i++){
-        for(let j = 0; j < nums.length; j++){
-    }
-            if((nums[i] == nums[j]) && (i < j)){
-        }
-                ans++
-            }
-    return ans
+function numIdenticalPairs(nums: number[]): number {
+    let ans:number = 0
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if((nums[i] == nums[j]) && (i < j)){
+                ans++
+            }
+        }
+    }
+    return ans
 };
