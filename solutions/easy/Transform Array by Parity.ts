94876465d3544af6f629474574759175da8cccac
Fidel Garcia Villa Jr. <54052260+fidelgarz67@users.noreply.github.com>
2025-08-07 09:56:57 -0600

    Add solution for Transform Array by Parity

---
+++ solutions/easy/Transform Array by Parity.ts
@@ -0,0 +1,20 @@
+// Title: Transform Array by Parity
+            // Difficulty: Easy
+            // Language: TypeScript
+            // Link: https://leetcode.com/problems/transform-array-by-parity/
+
+function transformArray(nums: number[]): number[] {
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i]%2 === 0){ 
+    }
+
+    for(let i = 0; i < nums.length; i++){
+            nums[i] = 0
+            }
+            console.log(nums[i])
+        if(nums[i]%2 !== 0){ 
+            nums[i] = 1
+            }
+    }
+    return nums.sort()
+};
