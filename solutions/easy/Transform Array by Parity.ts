7496f979f1a2cc78d3cb2f76e922ac04da64f541
Fidel Garcia Villa Jr. <54052260+fidelgarz67@users.noreply.github.com>
2025-08-07 09:58:37 -0600

    Update Transform Array by Parity.ts
    Update code and indention

--- solutions/easy/Transform Array by Parity.ts
+++ solutions/easy/Transform Array by Parity.ts
@@ -3,18 +3,10 @@
             // Language: TypeScript
             // Link: https://leetcode.com/problems/transform-array-by-parity/
 
-function transformArray(nums: number[]): number[] {
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i]%2 === 0){ 
-    }
-
-    for(let i = 0; i < nums.length; i++){
-            nums[i] = 0
-            }
-            console.log(nums[i])
-        if(nums[i]%2 !== 0){ 
-            nums[i] = 1
-            }
-    }
-    return nums.sort()
+function transformArray(nums: number[]): number[] {
+    const n = nums.length;
+    for (let i = 0; i < n; i += 1) {
+        nums[i] = (nums[i] % 2 === 0) ? 0 : 1;
+    }
+    return nums.sort((a, b) => a - b);    
 };
